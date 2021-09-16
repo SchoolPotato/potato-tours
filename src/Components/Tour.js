8fc1f84266feeb5a92ac8a4a8e3301e52254d29f
Alexander Welton <71471789+SchoolPotato@users.noreply.github.com>
2021-09-15 18:29:09 -0700

    Added non-functional "Not Interested" button, finished "Read More" button functionality

--- src/Components/Tour.js
+++ src/Components/Tour.js
@@ -1,10 +1,16 @@
+import {useState} from 'react';
+
 const Tour = ({name, info, image, price}) => {
+    const [readMore, setReadMore] = useState(false);
+    let readText = (readMore) ? "-Collapse" : "+Read More";
+    let collapsed = (readMore) ? info.trim() : info.substring(0, 180).trim() + "...";
     return (
         <div className="tour" style={{backgroundImage:image}}>
             <img className="tour" src={image} alt="whoops"/>
             <h5>${price}</h5>
             <h3>{name}</h3>
-            <p>{info} <a href={"#" + undefined} onClick={()=>{console.log("This doesn't work yet.")}}>-Read More</a></p>
+            <p>{collapsed} <a href={"#" + undefined} onClick={()=>{setReadMore(!readMore); console.log(readMore)}}>{readText}</a></p>
+            <button id="delete" onClick={()=>{console.log("Doesn't work yet")}}>Not Interested</button>
         </div>
     )
 }
